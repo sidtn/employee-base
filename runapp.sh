512099881a95c0b286c6191fb44b4650cb369372
sidtn <85740305+sidtn@users.noreply.github.com>
2022-01-26 22:03:50 +0300

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -5,15 +5,10 @@ ADMIN_EMAIL="admin@myproject.com"
 PASSWORD="password"
 
 python3 -m venv venv
-
-activate () {
-  . ./venv/bin/activate
-}
-
+source venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py loaddata professions.json
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); 
-\User.objects.create_superuser('$ADMIN_NAME', '$ADMIN_EMAIL', '$PASSWORD')" | python3 manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$ADMIN_NAME', '$ADMIN_EMAIL', '$PASSWORD')" | python3 manage.py shell
 python3 manage.py runserver
